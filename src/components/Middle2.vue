f59b77fe1b9f66dedb914a18ae411750b91abfe2
ctt123 <283569035@qq.com>
2018-05-18 11:00:41 +0800

    #app

--- src/components/Middle2.vue
+++ src/components/Middle2.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="middle3">
-    <img src="../assets/logo.png" alt="">
+  <div class="con">
+    <div class="middle3">
+      <img src="../assets/logo.png" alt="">
+    </div>
   </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-  #app {
+  .con {
     display: flex;
     justify-content: center;
     align-items: center;
-    height:100%;
-    width:100%;
+    height: 100%;
+    width: 100%;
   }
 </style>
