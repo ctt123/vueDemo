412cf44b8e41ee1ae2effd4545301a3bb23378b1
ctt123 <283569035@qq.com>
2018-05-21 11:31:34 +0800

    使用render

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,19 +1,5 @@
-<template>
-  <div class="cards">
-    <div v-for="item in cards" :key="item.id" class="list-item">
-      <my-card
-        :discount="item.discount"
-        :desc="item.desc"
-        :price="item.price"
-        :oPrice="item.oPrice"
-        :detail="item.detail"
-        :title="item.title">
-      </my-card>
-    </div>
-  </div>
-</template>
 
-<script>
+<script type="text/jsx">
   import MyCard from './Card.vue'
 
   export default {
@@ -24,6 +10,15 @@
         type: Array,
         required: true
       }
+    },
+    render () {
+      return (
+        <div className="cards">
+          {this.cards.map(item => <div className="list-item">
+            <MyCard {...item}/>
+          </div>)}
+        </div>
+      )
     }
   }
 </script>
