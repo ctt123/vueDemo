d89ec24f206859f539e9e6d42c32c2db2344a2b4
ctt123 <283569035@qq.com>
2018-06-01 16:16:43 +0800

    done

--- src/components/Test3.vue
+++ src/components/Test3.vue
@@ -1,22 +1,28 @@
 <template>
-  <div class="container">
-    <div class="tabs" ref="tabs">
-      <div class="tab" :class="{active:tab.isActive}" @click="goAnchor('#anchor-'+tab.id,index)"
-           v-for="(tab,index) in tabs" ref="tab"
-           :key="tab.id">
-        {{tab.name + index}}
-      </div>
-    </div>
+  <!--<div class="container">-->
+  <!--<div class="tabs" ref="tabs">-->
+  <!--<div class="tab" :class="{active:tab.isActive}" @click="goAnchor('#anchor-'+tab.id,index)"-->
+  <!--v-for="(tab,index) in tabs" ref="tab"-->
+  <!--:key="tab.id">-->
+  <!--{{tab.name + index}}-->
+  <!--</div>-->
+  <!--</div>-->
 
-    <div class="content-container" ref="con" @scroll="changeContent()">
-      <div class="content" v-for="tab in tabs" :id="'anchor-'+tab.id" :key="tab.id">
-        {{tab.content}}
-      </div>
-    </div>
-  </div>
+  <!--<div class="content-container" ref="con" @scroll="changeContent()">-->
+  <!--<div class="content" v-for="tab in tabs" :id="'anchor-'+tab.id" :key="tab.id">-->
+  <!--{{tab.content}}-->
+  <!--</div>-->
+  <!--</div>-->
+  <!--</div>-->
+  <Tab :tabs="tabs">
+
+  </Tab>
 </template>
 <script>
+  import Tab from './Tab.vue'
+
   export default {
+    components: {Tab},
     name: 'Test3',
     data () {
       return {
@@ -56,70 +62,68 @@
             name: 'G',
             content: 'g',
             isActive: false
-          }],
-        oIndex: 0,
-        tab: null,
-        contentHeightArr: []
+          }]
       }
-    },
-    methods: {
-      // 点击tab，滚动至对应div，tab变亮
-      goAnchor (selector, index) {
-        const contentContainer = this.$refs.con
-        contentContainer.scrollTop = this.contentHeightArr[index]
-        this.getLight(index)
-      },
-      // tab变亮
-      getLight (index) {
-        if (this.oIndex !== index) {
-          this.tabs[this.oIndex].isActive = false
-          this.oIndex = index
-          this.tabs[index].isActive = true
-        }
-      },
-      // tab居中
-      tabCenter (index) {
-        this.$refs.tabs.scrollLeft = this.$refs.tab[index].offsetLeft - document.body.clientWidth / 2 + this.$refs.tab[index].clientWidth / 2
-      },
-      // scroll滚动出发事件
-      changeContent () {
-        this.contentHeightArr = this.tabs.map(item => this.$el.querySelector('#anchor-' + item.id).offsetTop - 40)
-        const contentContainer = this.$refs.con
-        for (let i = 0; i < this.contentHeightArr.length; i++) {
-          if ((contentContainer.scrollTop >= this.contentHeightArr[i] - 1) && (contentContainer.scrollTop < this.contentHeightArr[i + 1] - 1)) {
-            this.getLight(i)
-            this.tabCenter(i)
-            return
-          }
-        }
-      }
-    },
-    mounted () {
-      this.contentHeightArr = this.tabs.map(item => this.$el.querySelector('#anchor-' + item.id).offsetTop - 40)
-//      console.log(document.body.clientWidth)
     }
   }
 </script>
 
 <style scoped>
-  .container {  display: flex;  flex-direction: column;  height: 100%;  font-size: 0;  }
+  .container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    font-size: 0;
+  }
 
-  .tabs {  flex: none;  background-color: #cccccc;  font-size: 0;  padding: 5px 10px;  overflow-x: scroll;  white-space: nowrap;  }
+  .tabs {
+    flex: none;
+    background-color: #cccccc;
+    font-size: 0;
+    padding: 5px 10px;
+    overflow-x: scroll;
+    white-space: nowrap;
+  }
 
-  .tab {  text-align: center;  background-color: #999;  color: #fff;  font-size: 12px;  display: inline-block;  width: 80px;  height: 30px;  margin-right: 10px;  line-height: 30px;  }
+  .tab {
+    text-align: center;
+    background-color: #999;
+    color: #fff;
+    font-size: 12px;
+    display: inline-block;
+    width: 80px;
+    height: 30px;
+    margin-right: 10px;
+    line-height: 30px;
+  }
 
-  .content {  min-height: 200px;  width: 100%;  -webkit-box-sizing: border-box;  -moz-box-sizing: border-box;  box-sizing: border-box;  background-color: #999999;  font-size: 12px;  margin-bottom: 10px;  }
+  .content {
+    min-height: 200px;
+    width: 100%;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    background-color: #999999;
+    font-size: 12px;
+    margin-bottom: 10px;
+  }
 
-  .content-container {  overflow: scroll;  flex: 1;  padding: 10px;  }
+  .content-container {
+    overflow: scroll;
+    flex: 1;
+    padding: 10px;
+  }
 
   /*.light-enter {*/
-    /*background: red;*/
-    /*-webkit-transition: background 5.5s;*/
-    /*-moz-transition: background 5.5s;*/
-    /*-ms-transition: background 5.5s;*/
-    /*-o-transition: background 5.5s;*/
-    /*transition: background 5.5s;*/
+  /*background: red;*/
+  /*-webkit-transition: background 5.5s;*/
+  /*-moz-transition: background 5.5s;*/
+  /*-ms-transition: background 5.5s;*/
+  /*-o-transition: background 5.5s;*/
+  /*transition: background 5.5s;*/
   /*}*/
 
-  .active {  background: aqua;  }
+  .active {
+    background: aqua;
+  }
 </style>
